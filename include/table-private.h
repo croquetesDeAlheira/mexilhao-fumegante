f135fa357817c1abd7e22064fb0b562c0a010148
sucramsaid <marcus-dias@hotmail.com>
2016-10-11 09:25:25 +0100

    init table

---
+++ include/table-private.h
@@ -0,0 +1,15 @@
+﻿#ifndef _TABLE_PRIVATE_H
+#define _TABLE_PRIVATE_H
+
+#include "list-h"
+
+struct table_t{
+	struct list_t tabela[]; //um array de list_t
+	int maxTabelaSize; // numero de linhas da tabela
+	int tabelaSize; // tamanho da tabela com linhas preenchidas
+}
+
+
+
+
+#endif
