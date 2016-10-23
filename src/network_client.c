78dbfb885e0e4b84767b57e847ce59b27f565806
danielfmsantos <santosmfdaniel@gmail.com>
2016-10-23 18:19:04 +0100

    n_c

--- src/network_client.c
+++ src/network_client.c
@@ -83,12 +83,34 @@ int network_close(struct server_t *server){
    através da socket sock.
 */
 int write_all(int sock, char *buf, int len){
-
+	int bufsize = len;
+	while(len > 0){
+		int res = write(sock, buf, len);
+		if(res < 0){
+			if(errno == EINTR) continue;
+			perror("write failed:");
+			return res;
+		}
+		buf+= res;
+		len-= res;
+	}
+	return bufsize;
 }
 
 /* Função que garante a receção de len bytes através da socket sock,
    armazenando-os em buf.
 */
 int read_all(int sock, char *buf, int len){
-
+	int bufsize = len;
+	while(len > 0){
+		int res = read(sock, buf, len);
+		if(res < 0){
+			if(errno == EINTR) continue;
+			perror("write failed:");
+			return res;
+		}
+		buf+= res;
+		len-= res;
+	}
+	return bufsize;
 }
